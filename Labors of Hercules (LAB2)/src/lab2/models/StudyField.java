031976d8b64fa54a2d2975e64c5530e24250aace
Garbuz Nelli <120444803+nelldino@users.noreply.github.com>
2023-10-04 14:41:09 +0300

    Delete Labors of Hercules (LAB2) directory

--- Labors of Hercules (LAB2)/src/lab2/models/StudyField.java
+++
@@ -1,9 +0,0 @@
-package src.lab2.models;
-
-public enum StudyField {
-    MECHANICAL_ENGINEERING,
-    SOFTWARE_ENGINEERING,
-    FOOD_TECHNOLOGY,
-    URBANISM_ARCHITECTURE,
-    VETERINARY_MEDICINE
-}
